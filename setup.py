a2c4a93d97a6cd48993e4c4bb9eacc0550822d4f
Philipp Schmitt <philipp@schmitt.co>
2017-05-30 11:31:37 +0200

    Version 1.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='restorepoint',
-    version='1.2.2',
+    version='1.3.0',
     license='GPL3',
     description='Python API wrapper for RestorePoint',
     # long_description=open('README.rst').read(),
@@ -11,7 +11,7 @@ setup(
     author_email='philipp@schmitt.co',
     url='https://github.com/pschmitt/python-restorepoint',
     packages=find_packages(),
-    install_requires=['requests', 'pathos'],
+    install_requires=['requests', 'pathos', 'python-dateutil'],
     entry_points={
         'console_scripts': ['rp=restorepoint.rp:main']
     }
